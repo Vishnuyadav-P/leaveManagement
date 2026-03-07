b0f57da148bd6b270745854c8c331b3360fd0188
Vishnu <vishnuyadavpullagura@gmail.com>
2026-03-07 17:01:23 +0530

    fixed api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://leavemanagement-ogne.onrender.com',
+  baseURL: 'https://leavemanagement-ogne.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
